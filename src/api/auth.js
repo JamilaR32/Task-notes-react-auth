7fde2f49097a4248f7500c26f1974e00ae2421dd
Jamila <j.r.cba@hotmail.com>
2024-01-08 16:59:58 +0300

    solve

--- src/api/auth.js
+++ src/api/auth.js
@@ -7,7 +7,9 @@ const login = async (userInfo) => {
 
 const register = async (userInfo) => {
   const formData = new FormData();
-  for (const key in userInfo) formData.append(key, userInfo[key]);
+  for (let key in userInfo) {
+    formData.append(key, userInfo[key]);
+  }
   const { data } = await instance.post("/auth/register", formData);
   return data;
 };
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,7 +3,10 @@ import { register } from "../api/auth";
 import { useMutation } from "@tanstack/react-query";
 const Register = () => {
   const [userInfo, setUserInfo] = useState({});
-
+  const { mutate: addFn } = useMutation({
+    mutationKey: ["register"],
+    mutationFn: () => register(userInfo),
+  });
   const handleChange = (e) => {
     if (e.target.name === "image") {
       setUserInfo({ ...userInfo, [e.target.name]: e.target.files[0] });
@@ -11,13 +14,9 @@ const Register = () => {
       setUserInfo({ ...userInfo, [e.target.name]: e.target.value });
     }
   };
-  const { mutate: addFn } = useMutation({
-    mutationKey: ["register"],
-    mutationFn: () => register(userInfo),
-  });
 
   const handleFormSubmit = (e) => {
-    // e.preventDefault();
+    e.preventDefault();
     // Add register logic here
     addFn();
   };
